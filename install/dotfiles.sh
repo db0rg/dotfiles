5f9c554e63955dbb4018ab176b73e39c786accf6
Devi Borg <devi.borg@gmail.com>
2020-01-06 22:25:47 +0100

    more complete installation that should work on clean machine

---
+++ install/dotfiles.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+function {
+	local original_directory=`pwd`
+	cd
+	local to_link_home=(zshrc zshenv tmux.conf)
+	local to_link_config=(kak kak-lsp lf)
+	for file in $to_link_home $to_link_config
+	do
+		local target=".$file"
+		local source="dotfiles/$file"
+		if [[ ${to_link_config[(ie)$file]} -le ${#to_link_config} ]]; then
+			local target=".config/$file"
+			local source="../dotfiles/$file"
+		fi
+		## need to check this first b'c otherwise get problem
+		#with files in subdirectories or pointing to non-existing
+		#files registering as nonexisting
+		if [[ -L "$target" ]]
+		then
+			rm "$target"
+		elif [[ -e "$target" ]]
+		then
+			mv "$target" "$target.backup$(date +'%Y%m%dT%H%M')"
+		fi
+		ln -s "$source" "$target"
+	done
+	cd "$original_directory"
+}
+
---
+++ install/mac.sh
@@ -0,0 +1,88 @@
+#!/bin/zsh
+#
+
+# build tools
+function {
+	if ! xcode-select --print-path &> /dev/null; then
+		xcode-select --install
+
+		until xcode-select --print-path &> /dev/null; do sleep 5; done
+
+		echo "XCode installed, accept the license with the power of root!"
+		sudo xcodebuild -license accept
+	fi
+	if ! which brew &> /dev/null; then
+		echo "Installing brew"
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+}
+
+# brew casks
+function {
+	local casks_to_install=(r rstudio osxfuse karabiner-elements java font-firacode-nerd-font scroll-reverser dash iTerm2)
+	for cask in $casks_to_install; do
+		if ! brew cask list $cask &> /dev/null; then
+			echo "Installing brew cask: $cask"
+			brew cask install "$cask"
+		fi
+	done
+}
+
+# brews
+function {
+	local brews_to_install=(kakoune tmux ripgrep bat lf graphviz zsh-autosuggestions zsh-syntax-highlighting)
+	local brews_to_install=($brews_to_install coreutils entr exa findutils gawk go inetutils jq ul/kak-lsp/kak-lsp)
+	local brews_to_install=($brews_to_install mawk rsync sshfs stdman tldr unrar wget)
+	for brew in $brews_to_install; do
+		if ! brew list $brew &> /dev/null; then
+			echo "Installing brew: $brew"
+			brew install "$brew"
+		fi
+	done
+}
+
+# language tools
+function {
+	if ! (echo 'library("languageserver")' | R --slave &> /dev/null) ; then
+		echo 'install.packages("languageserver", repos="https://cloud.r-project.org")' | R --slave
+	fi
+	GO111MODULE=on go get golang.org/x/tools/gopls@latest
+	if ! which goimports &> /dev/null; then
+		go get golang.org/x/tools/cmd/goimports
+	fi
+	local py3_installed="$(pip3 list)"
+	local py3_install=(pylint black pyls bpython numpy jupyter pandas matplotlib seaborn sklearn)
+	for pkg in $py3_install; do
+		if ! (printf '%s' "$py3_installed" | grep $pkg &>/dev/null); then
+			echo "pip3 install $pkg"
+			pip3 install $pkg
+		fi
+	done
+}
+
+# Defaults
+function {
+	mkdir -p ~/Library/KeyBindings
+	printf '{\n\t"^w" = "deleteWordBackward:";\n}' > ~/Library/KeyBindings/DefaultKeyBinding.dict
+
+	defaults write com.apple.dock tilesize -int 74
+	defaults write com.apple.dock minimize-to-application -bool true
+	defaults write com.apple.dock show-process-indicators -bool true
+	defaults write com.apple.dock mru-spaces -bool false
+	defaults write com.apple.dock autohide-delay -float 0
+	defaults write com.apple.dock autohide-time-modifier -float 0.5
+	defaults write com.apple.dock autohide -bool true
+	defaults write com.apple.dock magnification -bool false
+	killall Dock &> /dev/null
+
+	defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+	defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+	defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+	defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+	defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+	defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+	defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+	defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+}
+
